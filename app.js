bed9db38ccad3c280dbf2790aefe2df6d79d8df0
RyanmjDev <123763401+RyanmjDev@users.noreply.github.com>
2023-05-18 19:40:52 -0500

    cors change

--- app.js
+++ app.js
@@ -12,20 +12,23 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   .then(() => console.log('MongoDB connected.'))
   .catch(err => console.log(err));
 
-const whitelist = [' http://localhost:5173/',  'http://localhost:5174/',
-'https://ryandev-tasklister.netlify.app/'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
+// const whitelist = [' http://localhost:5173/',  'http://localhost:5174/',
+// 'https://ryandev-tasklister.netlify.app'];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200, 
+// };
 
-app.use(cors(corsOptions));
+// Allow all origins to access the server
+// we will change this later, but right for testing...
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,6 +43,8 @@ app.use('/api', routes);
 app.get('/', (req, res) => {
   res.send("The Server is running");
 })
+
+
 
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
